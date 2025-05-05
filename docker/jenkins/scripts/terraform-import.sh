b348d7d4edf635ee4fe05c5bc9b7603e3e8503aa
Ofir Gaash <77354079+ofirgaash1@users.noreply.github.com>
2025-05-05 16:35:34 +0300

    Update terraform-import.sh

--- docker/jenkins/scripts/terraform-import.sh
+++ docker/jenkins/scripts/terraform-import.sh
@@ -13,6 +13,9 @@ if [ $? -eq 0 ]; then
   exit 0
 fi
 
+
 echo "[INFO] Importing ECS service into Terraform state..."
 terraform import aws_ecs_service.my_service imtech/ofir || exit 1
+echo "[INFO] Importing ALB listener into Terraform state..."
+terraform import aws_lb_listener.http arn:aws:elasticloadbalancing:il-central-1:314525640319:listener/app/imtec/dd67eee2877975d6/e8f47476687e464a || exit 1
 
